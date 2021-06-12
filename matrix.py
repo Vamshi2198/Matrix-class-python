c472c267c4aff9a25f1b6a5f0240ed33cba297bf
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-06-11 21:45:01 -0400

    initialize the matrix class

---
+++ matrix.py
@@ -0,0 +1,16 @@
+import math
+from math import sqrt
+import numbers
+class Matrix(object):
+
+    # Constructor
+    def __init__(self, grid):
+        self.g = grid
+        self.h = len(grid)
+        self.w = len(grid[0])
+
+    #
+    # Primary matrix math methods
+    #############################
+ 
+ 
