e58cc377584112adbdcb45d156a0a2448fd2a2dd
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-06-11 22:09:42 -0400

    created determinant function

--- matrix.py
+++ matrix.py
@@ -13,4 +13,17 @@ class Matrix(object):
     # Primary matrix math methods
     #############################
  
- 
+ def determinant(self):
+        """
+        Calculates the determinant of a 1x1 or 2x2 matrix.
+        """
+        if not self.is_square():
+            raise(ValueError, "Cannot calculate determinant of non-square matrix.")
+        if self.h > 2:
+            raise(NotImplementedError, "Calculating determinant not implemented for matrices largerer than 2x2.")
+            
+        a = self.g[0][0]
+        b = self.g[0][1]
+        c = self.g[1][0]
+        d = self.g[1][1]   
+        return (a * d - b * c) 
