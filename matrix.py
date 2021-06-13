8767f803f78087003aff7eb21e629cbe33d930b4
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-06-12 23:48:46 -0400

    defined identity function

--- matrix.py
+++ matrix.py
@@ -9,6 +9,15 @@ def zeroes(height, width):
         g = [[0.0 for _ in range(width)] for __ in range(height)]
         return Matrix(g)
 
+def identity(n):
+        """
+        Creates a n x n identity matrix.
+        """
+        I = zeroes(n, n)
+        for i in range(n):
+            I.g[i][i] = 1.0
+        return I
+
 class Matrix(object):
 
     # Constructor
